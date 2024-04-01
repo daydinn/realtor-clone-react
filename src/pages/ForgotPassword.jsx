5710930f29bcb7a58571e60269aaee2b5a9a5bd8
daydinn <58174442+daydinn@users.noreply.github.com>
2024-04-01 03:54:12 +0200

    git commit complete execpt this one bug with sendPasswordResetEmail

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 
 export default function ForgotPassword() {
 
@@ -11,6 +13,25 @@ export default function ForgotPassword() {
   function onChange(e) {
    
    setEmail(e.target.value);
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    
+    try{
+      const auth= getAuth()
+      await sendPasswordResetEmail(auth,email);
+      toast.success("Email was sent");
+
+    }
+    catch(error){
+    toast.error("Could not send reset password");
+    
+
+
+    }
+
+
   }
 
   return (
@@ -25,7 +46,7 @@ export default function ForgotPassword() {
           ></img>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
           
             <input
               
