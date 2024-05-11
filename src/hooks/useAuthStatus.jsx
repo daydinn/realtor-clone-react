7d5cb8f03811ecf42e68c09ebaa5559679e0f8c1
daydinn <58174442+daydinn@users.noreply.github.com>
2024-05-11 23:47:34 +0200

    Create the slider component and add it to the home page

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+export function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
 
-export  function useAuthStatus() {
-  
-const [loggedIn, setLoggedIn]= useState(false);
-const [checkingStatus, setCheckingStatus] = useState(true);
-
-//ask if person is authenticated or not
-useEffect(() => {
-const auth = getAuth();
-//we ar going to check if person authenticated with onAuthStateChanged
-onAuthStateChanged(auth, (user) =>{
-    //we are going to set loggedIn true if person is authenticated
-    if(user){
+  useEffect(() => {
+    const auth = getAuth();
+    console.log(auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
         setLoggedIn(true);
-    }
-    //after checkin we set it to false which was default true
-    setCheckingStatus(false);
-});
-
-
-
-}, []);
-return{ loggedIn, checkingStatus }; //we are going to use these 2 information inside of the private-route
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
 }
-
 
 
